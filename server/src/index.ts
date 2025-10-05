bd379b4258a7dc3da37a36df752a54b1cfa82397
shammy14-1 <shammysuyat@gmail.com>
2025-10-06 07:49:44 +0800

    chore: setup dependencies, node server

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+console.log("server is running");
