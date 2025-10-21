c3fd18de06767e2c478ca39f2acd018c5af490fe
shammy14-1 <shammysuyat@gmail.com>
2025-10-21 21:24:19 +0800

    feat, refactor: reservation, customer, types

--- server/src/reservation/reservation.controller.ts
+++ server/src/reservation/reservation.controller.ts
@@ -4,13 +4,16 @@ import { reservationService } from "./reservation.service";
 class ReservationController {
   async createReservation(req: Request, res: Response) {
     try {
-      const { data } = req.body;
-
-      const email = data.email;
+      const data = req.body;
 
       if (!data) return res.status(400).json({ message: "no data fetched" });
 
-      const result = await reservationService.createReservation(email);
+      const result = await reservationService.createReservation(data);
+      console.info("reservation supabase result: ", result);
+
+      return res
+        .status(200)
+        .json({ message: "Reservation Created Successfully! ", result });
     } catch (error: any) {
       console.error("error from createReservation(): ", error);
       return res.status(400).json({ message: "can't create reservation" });
---
+++ server/src/types/Reservation.ts
@@ -0,0 +1,12 @@
+export type ReservationData = {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  mobileNumber?: string;
+  validIdUrl?: string;
+  paymentUrl?: string;
+  reservationType?: string;
+  date?: string;
+  pax?: number;
+  reservationAmount: number;
+};
