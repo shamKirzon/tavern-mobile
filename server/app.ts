cda0f8127ee768d3a7229d9fe670abb15ba0aa4a
shammy14-1 <shammysuyat@gmail.com>
2025-11-08 19:07:56 +0800

    chore, feat: rateLimiter setup, limits incoming requests

--- server/app.ts
+++ server/app.ts
@@ -5,10 +5,20 @@ import customerRoutes from "./src/customer/customer.routes";
 import reservationRoutes from "./src/reservation/reservation.routes";
 import orderRoutes from "./src/order/order.route";
 import authRoutes from "./src/auth/auth.routes";
+import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
+
+const rateLimiter: RateLimitRequestHandler = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+  message: { message: "Too many request from this IP, please try again later" },
+  standardHeaders: true,
+  legacyHeaders: true,
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/otp", otpRoutes);
 app.use("/api/customer", customerRoutes);
