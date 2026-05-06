f6d360ff34b3be5f434e044657136e3d4fc9c67e
shammy14-1 <shammysuyat@gmail.com>
2026-05-06 22:07:00 +0800

    chore: update dependencies and switch axios baseURL to ngrok

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import IP_ADDRESS from "../constant/IpAddress";
 
+const NGROK_URL = "https://nondehiscent-medfly-joslyn.ngrok-free.dev";
+
 export const axiosInstance = axios.create({
-  baseURL: `${IP_ADDRESS}/api`,
+  // baseURL: `${IP_ADDRESS}/api`,
+  baseURL: `${NGROK_URL}/api`,
   withCredentials: true,
 });
